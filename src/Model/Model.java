8290374919dddfcd1f8a0b99ff0b8c48b760be80
pppog <77719393+pppog@users.noreply.github.com>
2023-01-12 13:44:30 +0100

    check point working

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,6 +1,8 @@
 package Model;
 import View.*;
 
+import java.util.Arrays;
+
 public class Model {
     int width;
     int height;
@@ -11,8 +13,27 @@ public class Model {
 
     public void update(Point[] points) {
         for (int i = 0; i<points.length; i++) {
-            System.out.println(getGranne(width, height, points));
+            getGrannar(points);
         }
+    }
+
+    public int getGrannar(Point[] points) {
+        int grannar = -1;
+
+        for (int x = 0; x<points.length; x++) {
+        for (int i = 0; i<points.length; i++) {
+            if (points[x].getX() - points[i].getX() <= 1 && points[x].getX() - points[i].getX() >= -1 && points[x].getY() - points[i].getY() <= 1 && points[x].getY() - points[i].getY() >= -1) {
+                grannar++;
+            }
+        }
+            System.out.println(x+1 + " has " + grannar + " grannar");
+        if(grannar < 2 || grannar > 3) {
+            System.out.println(points[x] + " should be dead");
+            // remove that bih
+        }
+        grannar = -1;
+        }
+        return grannar;
     }
 
     public int getGranne(int width, int height, Point[] points) {
@@ -20,26 +41,31 @@ public class Model {
         int x;
         int y;
 
-        for (int i = 1; i<height; i++) {
-            y=i;
-            x=1;
-            for (int o = 1; o<width; o++) {
-                if (x - points[i].getX() <= 1 && x - points[i].getX() >= -1 && y - points[i].getY() <= 1 && y - points[i].getY() >= -1) {
-                    grannar++;
+        for (int i = 0; i<points.length; i++) {
+            for (int p = 1; p < height; p++) {
+                y = p;
+                x = 1;
+                for (int o = 2; o < width; o++) {
+                    if (x - points[i].getX() <= 1 && x - points[i].getX() >= -1 && y - points[i].getY() <= 1 && y - points[i].getY() >= -1) {
+                        grannar++;
+                    }
+                    x = o;
                 }
             }
+            System.out.println(i+1 + " has " + grannar + " grannar");
+            grannar = 0;
         }
 
         return grannar;
     }
 
     public Point[] getPoints() {
-        Point[] points = {new Point(7,7), new Point(7,8), new Point(8,8)};
+        Point[] points = {new Point(7,7), new Point(7,8), new Point(8,8), new Point(10,8), new Point(11,8)};
 
         return points;
     }
     public Shape[] getShapes() {
-        Point[] points = {new Point(7,7), new Point(7,8), new Point(8,8)};
+        Point[] points = {new Point(7,7), new Point(7,8), new Point(8,8), new Point(10,8), new Point(11,8)};
 
         return (Shape[])points;
     }
